b85e9f6d48d65d3e2cd1e6248a774f54a4fac1ac
Qs-F <qsf@de-liker.com>
2022-03-02 01:17:10 +0900

    Fmt

--- src/index.ts
+++ src/index.ts
@@ -12,17 +12,17 @@ const walk = async <StackNode>(
     return
   }
   await Promise.all(
-    root.children.map(async (child) => {
-      await walk(child, f)
-    })
+      root.children.map(async (child) => {
+        await walk(child, f)
+      })
   )
 }
 
 const apply = async (node: TextNode, text: string): Promise<boolean> => {
   await Promise.all(
-    node.getRangeAllFontNames(0, node.characters.length).map(async (font) => {
-      await figma.loadFontAsync(font)
-    })
+      node.getRangeAllFontNames(0, node.characters.length).map(async (font) => {
+        await figma.loadFontAsync(font)
+      })
   )
   node.characters = text
   return true
@@ -39,66 +39,68 @@ const main = async () => {
   }
 
   await Promise.all(
-    nodes.map(async (node) => {
-      if (!('fills' in node)) {
-        figma.notify('😱 No fill')
-        return
-      }
-      if (node.fills === figma.mixed) {
-        figma.notify('😱 Color is mixed (only 1 fill is allowed)')
-        return
-      }
-      if (node.fills.length < 1) {
-        figma.notify('😱 Multiple colors are set (only 1 fill is allowed)')
-        return
-      }
-      if (node.fills[0]?.type !== 'SOLID') {
-        figma.notify('😱 Color is not solid (probably gradient or image is set)')
-        return
-      }
-      const styleName =
+      nodes.map(async (node) => {
+        if (!('fills' in node)) {
+          figma.notify('😱 No fill')
+          return
+        }
+        if (node.fills === figma.mixed) {
+          figma.notify('😱 Color is mixed (only 1 fill is allowed)')
+          return
+        }
+        if (node.fills.length < 1) {
+          figma.notify('😱 Multiple colors are set (only 1 fill is allowed)')
+          return
+        }
+        if (node.fills[0]?.type !== 'SOLID') {
+          figma.notify(
+              '😱 Color is not solid (probably gradient or image is set)'
+          )
+          return
+        }
+        const styleName =
         (node.fillStyleId !== '' &&
           node.fillStyleId !== figma.mixed &&
           figma.getStyleById(node.fillStyleId)?.name) ||
         ''
-      const alpha = node.fills[0].opacity || 1
-      const fill = new Color(
-        Object.entries(node.fills[0].color).map(([, v]) => v * 255),
-        'rgb'
-      )
-      const fillA = new Color(
-        [
-          ...Object.entries(node.fills[0].color).map(([, v]) => v * 255),
-          parseFloat(alpha.toFixed(4)),
-        ],
-        'rgb'
-      )
-      await walk(
-        node,
-        async (node): Promise<boolean> => {
-          if (node.type !== 'TEXT') {
-            return true
-          }
-          switch (node.name) {
-            case '$hex':
-              return apply(node, fill.hex())
-            case '$hsl':
-              return apply(node, fill.hsl().round().string())
-            case '$hsla':
-              return apply(node, fillA.hsl().round().string())
-            case '$rgb':
-              return apply(node, fill.rgb().round().string())
-            case '$rgba':
-              return apply(node, fillA.rgb().round().string())
-            case '$alpha':
-              return apply(node, alpha.toFixed(4))
-            case '$styleName':
-              return apply(node, styleName)
-          }
-          return true
-        }
-      )
-    })
+        const alpha = node.fills[0].opacity || 1
+        const fill = new Color(
+            Object.entries(node.fills[0].color).map(([, v]) => v * 255),
+            'rgb'
+        )
+        const fillA = new Color(
+            [
+              ...Object.entries(node.fills[0].color).map(([, v]) => v * 255),
+              parseFloat(alpha.toFixed(4)),
+            ],
+            'rgb'
+        )
+        await walk(
+            node,
+            async (node): Promise<boolean> => {
+              if (node.type !== 'TEXT') {
+                return true
+              }
+              switch (node.name) {
+                case '$hex':
+                  return apply(node, fill.hex())
+                case '$hsl':
+                  return apply(node, fill.hsl().round().string())
+                case '$hsla':
+                  return apply(node, fillA.hsl().round().string())
+                case '$rgb':
+                  return apply(node, fill.rgb().round().string())
+                case '$rgba':
+                  return apply(node, fillA.rgb().round().string())
+                case '$alpha':
+                  return apply(node, alpha.toFixed(4))
+                case '$styleName':
+                  return apply(node, styleName)
+              }
+              return true
+            }
+        )
+      })
   )
   figma.closePlugin('Done ✨')
 }
